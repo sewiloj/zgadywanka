c32b80e877f0dfb8e1af2f00f00c1f7cc8e4dda8
Krzysztof Molenda <kmolenda@wsei.edu.pl>
2019-03-30 18:50:29 +0100

    Jeden ruch w grze

--- GraZaDuzoZaMalo/GraMonolitycznie/Program.cs
+++ GraZaDuzoZaMalo/GraMonolitycznie/Program.cs
@@ -10,10 +10,37 @@ namespace GraMonolitycznie
     {
         static void Main(string[] args)
         {
+            /*
             Console.WriteLine("Witaj!");
             Console.Write("Podaj swoje imię: ");
             string x = Console.ReadLine();
             Console.WriteLine($"Witaj, {x}");
+            */
+
+            // 1. Komputer losuje liczbę
+            Random generator = new Random();
+            int wylosowana = generator.Next(1, 101);
+            Console.WriteLine("Wylosowałem liczbę od 1 do 100. \n Odgadnij ją");
+
+#if(DEBUG)
+            Console.WriteLine(wylosowana);
+#endif
+
+            // 2. Człowiek proponuje rozwiązanie
+            Console.Write("Podaj swoją propozycję: ");
+            string tekst = Console.ReadLine();
+            int propozycja = Convert.ToInt32(tekst);
+            Console.WriteLine($"Przyjąłem wartość {propozycja}");
+
+            // 3. Komputer ocenia propozycję
+            if( propozycja < wylosowana )
+                Console.WriteLine("za mało");
+            else if( propozycja > wylosowana )
+                Console.WriteLine("za dużo");
+            else
+                Console.WriteLine("trafiono");
+
+            Console.WriteLine("Koniec gry");
         }
     }
 }

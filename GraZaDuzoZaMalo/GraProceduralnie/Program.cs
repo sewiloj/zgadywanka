272f07c5e490ec952e52d6c1a20b393f0adcfd1f
Sewiło Jakub <jakub.sewilo@microsoft.wsei.edu.pl>
2019-05-04 19:20:07 +0200

    Proceduralnie

--- GraZaDuzoZaMalo/GraProceduralnie/Program.cs
+++ GraZaDuzoZaMalo/GraProceduralnie/Program.cs
@@ -8,68 +8,91 @@ namespace GraProceduralnie
 {
     class Program
     {
-            int Losuj(int min, int max)
-            {
-                Random generator = new Random();
-                int wylosowana = generator.Next(min, max + 1);
-                return 0;
-            }
-        static void Main(string[] args)
+        /// <summary>
+        /// Wczytuje dane (liczba całkowita) z konsoli. Jeśli 'x' zgłasza wyjątek
+        /// </summary>
+        /// <param name="prompt">Tekst zachęty wyświetlany użytkownikowi</param>
+        /// <returns>liczba odczytana ze standardowego wejścia</returns>
+        /// <exception cref="InvalidOperationException">Gdy uzytkownik poda x</exception>
+        private static int WczytajDane(string prompt = "Podaj liczbę lub X jeśli koniec: ")
         {
-            
+
+            int propozycja = 0;
+
+            do
+            {
+                Console.Write(prompt);
+                string tekst = Console.ReadLine();
+                if (tekst.ToLower() == "x")
+                    throw new InvalidOperationException();
+                try
+                {
+                    propozycja = Convert.ToInt32(tekst);
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Nie podano liczby! Spróbuj jeszcze raz");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Liczba nie mieści się w rejestrze! Spróbuj jeszcze raz");
+                }
 
 
+            } while ( true );
+
+            return propozycja;
+        }
+        private static int Losuj(int min = 1, int max = 100)
+        {
             Random generator = new Random();
-            int wylosowana = generator.Next(1, 101);
+            int wylosowana = generator.Next(min, max + 1);
+            return wylosowana;
+        }
+
+        private static int wylosowana = 0;
+
+        private static string Ocena(int propozycja)
+        {
+            if (propozycja < wylosowana)
+                return "za mało";
+            else if (propozycja > wylosowana)
+               return "za dużo";
+            else
+               return "trafiono";
+        }
+
+
+        public static void Main(string[] args)
+        {
+
+            wylosowana = Losuj(1, 100); // moze byc zgloszony wyjatek, gdy min > max
             Console.WriteLine("Wylosowałem liczbę od 1 do 100. \n Odgadnij ją");
 
 #if(DEBUG)
             Console.WriteLine(wylosowana);
 #endif
-
-            //wykonuj
-            bool trafiono = false; //wartownik (zwany czasami flagą)
+            int propozycja = 0;
             do
             {
-                #region Krok 2. Człowiek proponuje rozwiązanie
-                Console.Write("Podaj swoją propozycję: ");
-                string tekst = Console.ReadLine();
-                if (tekst.ToLower() == "x")
-                    break;
-
-                int propozycja = 0;
                 try
                 {
-                    propozycja = Convert.ToInt32(tekst);
+                propozycja = WczytajDane("Podaj swoją propozycję (X - gdy koniec)");
                 }
-                catch (FormatException)
+                catch(InvalidOperationException)
                 {
-                    Console.WriteLine("Nie podano liczby!");
-                    continue;
-                }
-                catch (OverflowException)
-                {
-                    Console.WriteLine("Liczba nie mieści się w rejestrze!");
-                    continue;
+                    Console.WriteLine("Szkoda, że kończymy. Do widzenia.");
+                    return;
                 }
 
                 Console.WriteLine($"Przyjąłem wartość {propozycja}");
-                #endregion
-
-
-                #region Krok 3. Komputer ocenia propozycję
-                if (propozycja < wylosowana)
-                    Console.WriteLine("za mało");
-                else if (propozycja > wylosowana)
-                    Console.WriteLine("za dużo");
-                else
-                {
-                    Console.WriteLine("trafiono");
-                    trafiono = true;
-                }
-                #endregion
+                string ocena = Ocena(propozycja);
+                Console.WriteLine(ocena);
+                if (ocena == "trafiono")
+                    break;
             }
-            while (!trafiono);
+            while (true);
             //do momentu trafienia
 
             Console.WriteLine("Koniec gry");

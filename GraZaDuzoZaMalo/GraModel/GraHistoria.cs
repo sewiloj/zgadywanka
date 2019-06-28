b84e39c60df5b9e599a30f76075e3b72c027b3d2
Jakub Sewilo <jakub.sewilo@microsoft.wsei.edu.pl>
2019-06-28 10:32:00 +0200

    Add GraHistoria.cs

---
+++ GraZaDuzoZaMalo/GraModel/GraHistoria.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraModel
+{
+    public partial class Gra
+    {
+        public List<Ruch> Historia;
+        public class Ruch
+        {
+            public readonly int Propozycja;
+            public readonly Odp Odpowiedz;
+            public readonly DateTime Kiedy;
+
+            public Ruch(int prop, Odp odp)
+            {
+                Propozycja = prop;
+                Odpowiedz = odp;
+                Kiedy = DateTime.Now;
+            }
+
+            public override string ToString() => $"({Propozycja}, {Odpowiedz}, {Kiedy})";
+        }
+
+
+    }                                  
+}
